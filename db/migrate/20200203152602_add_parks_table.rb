578fbc1fcfa508933c4cffc39dd1033362ab608b
Eva Wiedmann <evawiedmann@gmail.com>
2020-02-03 08:30:10 -0800

    more migrations

--- db/migrate/20200203152602_add_parks_table.rb
+++ db/migrate/20200203152602_add_parks_table.rb
@@ -1,4 +1,10 @@
 class AddParksTable < ActiveRecord::Migration[5.2]
   def change
+    def change
+      create_table :parks do |t|
+        t.string :name
+        t.string :city
+        t.string :state
+      end
+    end
   end
-end
