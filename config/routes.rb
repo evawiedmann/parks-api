945228313c23dac5f1540b08903f32c61a6e1b72
Eva Wiedmann <evawiedmann@gmail.com>
2020-02-03 08:22:29 -0800

    migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-    mount Rswag::Api::Engine => '/api-docs'
+  # mount Rswag::Ui::Engine => '/api-docs'
+  #   mount Rswag::Api::Engine => '/api-docs'
     resources :parks do
       resources :reviews
     end
---
+++ db/migrate/20200203152614_add_reviews_table.rb
@@ -0,0 +1,4 @@
+class AddReviewsTable < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
